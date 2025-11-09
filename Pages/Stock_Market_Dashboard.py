d8c562cf3e1b7f6d37459749cd4785e89fe2d6bd
kaiyasymone <kaiyaevans0923@gmail.com>
2025-11-09 15:18:21 -0500

    Update Stock_Market_Dashboard.py

--- Pages/Stock_Market_Dashboard.py
+++ Pages/Stock_Market_Dashboard.py
@@ -19,3 +19,18 @@ st.markdown("---")
 symbol = st.text_input("Enter a stock symbol (ex. AAPL, TSLA, MSFT)", "AAPL").upper()
 num_points = st.slider("Number of recent 60-minute points", min_value = 10, max_value = 100, value = 40, help = "Each point represents one hour of trading data.")
 
+api_key = "ATBPU1SAP8PL9AN6"
+function = "TIME_SERIES_INTRADAY"
+interval = "60min"
+    
+url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&apikey={api_key}"
+response = requests.get(url)
+data = response.json()
+#query? string helps tell the API exactly what data you want
+# separate each parameter with &. Each parameter will have a key and a value
+
+#3 required parameters for Stock Market API:
+    #function(type of data,TIME_SEIRES_INTRADAY)
+    #symbol
+    #interval
+
