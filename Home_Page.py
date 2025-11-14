b5d25247ade978caeb8338a55307e8fccb4908e9
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2025-11-14 12:41:21 -0500

    Clean up Home_Page.py and set page configuration
    Removed commented-out code and added streamlit configuration.

--- Home_Page.py
+++ Home_Page.py
@@ -1,39 +1,6 @@
 # https://webdevlab03-team15.streamlit.app/
 # API Key (Alpha Vantage): 2GLDYP292HNQFQUP
 
-# import streamlit as st
-
-# # Title of App
-# st.title("Web Development Lab03")
-# st.header("Money Moves: Stock Data Explorer")
-
-# st.image("Images/intro_pic.jpg", use_container_width=True)
-
-# # Assignment Data 
-# # TODO: Fill out your team number, section, and team members
-
-# st.header("CS 1301")
-# st.subheader("Team 15, Web Development - Section D")
-# st.subheader("Team Member 1: Kaiya Evans, Team Member 2: Jenna Tran")
-
-
-# Introduction
-# TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
-#       2. **Page Name**: Description
-#       3. **Page Name**: Description
-#       4. **Page Name**: Description
-
-# st.write("""
-# Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
-
-# 1. Home Page: Introduces the project, its purpose, and provides navigation to the other pages.
-# 2. Stock Market Dashboard: Displays real-time market data and interactive visualizations of selected stocks.
-# 3. Stock Analysis: Analyzes historical performance metrics and trends to evaluate investment potential.
-# 4. Personal Stock Advisory: Offers personalized insights and portfolio suggestions based on user preferences and data.
-
-# """)
-
 import streamlit as st
 
 st.set_page_config(
