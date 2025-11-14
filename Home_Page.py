570c7f85d37a94696927c3232f3ae819a28c7e72
kaiyasymone <kaiyaevans0923@gmail.com>
2025-11-14 12:54:16 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -17,7 +17,7 @@ with st.container():
         st.image("Images/intro_pic.jpg", use_container_width=True)
 
     with right:
-        st.title("Money Moves")
+        st.title("MONEY MOVES")
         st.subheader("Stock Data Explorer")
         st.write("**CS 1301 — Team 15, Section D**")
         st.write("Team Members: **Kaiya Evans** & **Jenna Tran**")
