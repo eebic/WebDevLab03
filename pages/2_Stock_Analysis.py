ee1c9fb8e8f4df5866d33758333e59aee33436e9
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2025-11-16 14:13:19 -0500

    Update 2_Stock_Analysis.py

--- pages/2_Stock_Analysis.py
+++ pages/2_Stock_Analysis.py
@@ -1,1 +1,8 @@
+# Gemini API key: AIzaSyAESi3Zed0GIx4IgSzrynI5zTdxrDRWsiE
+
+import streamlit as st
+import requests
+import pandas as pd
+import google.generativeai as genai
+import datetime as dt
 
